b6a6ab1477de9a2dad508b37db12498c6dcb98c1
Edgar <edgarkrit@gmail.com>
2020-03-17 12:36:25 +0200

    added/renamed a few packages

--- src/main/java/com/softserve/message_sender/controller/UserReceiptController.java
+++ src/main/java/com/softserve/message_sender/controller/UserReceiptController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/user-receipt")
-public class MessageController {
+public class UserReceiptController {
 
     @Value("${paging.default-page-size}")
     private int pageSize;

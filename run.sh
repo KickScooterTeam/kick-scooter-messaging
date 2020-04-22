2e0bc099a7079145fb2f77fb8510fb031cfe70c8
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-04-22 17:29:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!bin/bash
 
-nohup java -jar *.jar </dev/null &>/dev/null &
-
-sleep 5
+java -jar *.jar &>/dev/null &
 
 ./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=messaging
